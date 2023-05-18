884dd9c9dcbea85900ef94982e8784660cf0a114
Matias Sotelo <matysotelo07@gmail.com>
2023-05-17 21:21:17 -0300

    actualizacion desarrollo

--- src/scripts.js
+++ src/scripts.js
@@ -64,6 +64,8 @@ function agregarCategoriasMenu() {
     
       // Funcionalidad botones del menu
       btnId.addEventListener("click", function() {
+        
+        contenedorProductos.style.display = "block"
 
         x.style.display = "none";
 
@@ -90,7 +92,7 @@ function agregarCategoriasMenu() {
         contenedorProductos.appendChild(headerCategoDom);
         
         //muestra los productos en el dom
-        function muestraProductosDom() {} {
+        function muestraCategoriasMenu() {} {
           producto.forEach(p => {
             const contProducto = document.createElement("div");
 
@@ -108,13 +110,14 @@ function agregarCategoriasMenu() {
           })
         }
         
-         muestraProductosDom();
+        muestraCategoriasMenu();
         
         // cerrar cabecera categoria
         const btnCerrarCatego = document.querySelector(".btn-cerrar-categoria");
         btnCerrarCatego.addEventListener("click", function() {
           contenedorProductos.innerHTML = '';
           contenedorProductos.style.border = 'none';
+          contenedorProductos.style.display = "none"
         })
 
       })
@@ -123,3 +126,80 @@ function agregarCategoriasMenu() {
 }
 
 agregarCategoriasMenu();
+
+const contCategorias = document.querySelector(".cont-principal-categorias");
+
+categorias.forEach(prod => {
+
+    const cardCategoria = document.createElement("div");
+    cardCategoria.classList.add("card-categoria")
+
+    cardCategoria.innerHTML = `
+        <h2>${prod.categoria}</h2>
+        <img src="assets/img/productos-img/${prod.img}.jpg" alt="${prod.categoria}">
+        <button type="submit" id="id-${prod.id}">Ver mas</button>
+    `
+    contCategorias.appendChild(cardCategoria);
+    
+    const btnId = cardCategoria.querySelector(`#id-${prod.id}`);
+
+    btnId.addEventListener('click', function() {
+      
+      contenedorProductos.style.display = "block"
+
+      const x = funcionalidadHamburguesa();
+
+      // agregaProductosCategoria
+      function agregarProductosCategoria () {
+        let codificarCategoria = categorias[prod.id].categoria;
+
+        const mostrarProductos = catalogo.filter(prod => prod.categoria ===  codificarCategoria)
+
+        console.log(`click ${prod.id}`)
+
+        return mostrarProductos;
+      }
+      
+      const producto = agregarProductosCategoria();
+
+      // crea cabecera categoria 
+      const headerCategoDom = document.createElement("div");
+      headerCategoDom.classList.add("cont-categoria");
+      headerCategoDom.innerHTML = `
+        <h2>${prod.categoria}</h2>
+        <button class="btn-cerrar-categoria" type="button">X</button>
+      `
+      contenedorProductos.appendChild(headerCategoDom);
+      
+      //muestra los productos en el dom
+      function muestraCategoriasMenu() {} {
+        producto.forEach(p => {
+          const contProducto = document.createElement("div");
+
+          contProducto.innerHTML += `
+            <div class="card-produco"> 
+              <h2>${p.nombreProducto}</h2>
+            </div>
+          `
+          
+          console.log(p.categoria)
+          
+          contenedorProductos.appendChild(contProducto);
+          contenedorProductos.style.border = '1px solid #8080805e';
+
+        })
+      }
+      
+      muestraCategoriasMenu();
+      
+      // cerrar cabecera categoria
+      const btnCerrarCatego = document.querySelector(".btn-cerrar-categoria");
+      btnCerrarCatego.addEventListener("click", function() {
+        contenedorProductos.innerHTML = '';
+        contenedorProductos.style.border = 'none';
+        contenedorProductos.style.display = "none"
+      })
+
+    })
+
+});
