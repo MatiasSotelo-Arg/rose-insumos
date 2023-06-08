46dfd149bf67a5322975814a1e1e872056f9c286
Matias Sotelo <matysotelo07@gmail.com>
2023-06-08 11:45:36 -0300

    actualizacion carrito

--- src/confirmarCompra.js
+++ src/confirmarCompra.js
@@ -1,3 +1,63 @@
+// let mensajeSaludo = "Hola mi pedido es:%0A%0A";
+// let mensajeDetalle = "";
+// let mensajeCompleto = "";
+// let mensajeEnviado = false; // Variable para controlar si el mensaje ya se envió
+
+// function confirmarCompra() {
+//     const contenedor = document.querySelector(".contenido-carrito");
+
+//     let btnConfirmarCompra = document.createElement("a");
+//     btnConfirmarCompra.classList.add("confirmar-compra");
+//     btnConfirmarCompra.textContent = "Confirmar Compra ✓"
+//     btnConfirmarCompra.href = "#"
+
+//     contenedor.appendChild(btnConfirmarCompra);
+
+//     detalleCompra(btnConfirmarCompra);
+// }
+
+// function detalleCompra(btnConfirmar) {
+//     btnConfirmar.addEventListener('click', () => {
+    
+//         if(!mensajeEnviado) {
+//             mensajeDetalle = "";
+
+//         carrito.forEach(prod => {
+//             mensajeDetalle += `*${prod.nombreProducto}*%0A${prod.cantidad} x $${prod.precio} - subTotal: $${prod.precioTotal}%0A%0A`;
+//         });
+
+//         const obtenerTotal = obtenerTotalCarrito()
+
+//         mensajeCompleto = `${mensajeSaludo}${mensajeDetalle}%0A%0ATotal: $${obtenerTotal}`;
+
+//         console.log(mensajeCompleto);
+
+//         Swal.fire({
+//             position: 'center',
+//             icon: 'success',
+//             title: 'Pedido Confirmado!',
+//             showConfirmButton: false,
+//             timer: 500
+//         })
+
+//         mensajeEnviado = true; // Establecer la variable de mensajeEnviado a true para evitar el bucle de alertas
+//         }
+        
+//         setTimeout(() => {
+//         btnConfirmar.target = "_blank"
+//         btnConfirmar.href = `https://api.whatsapp.com/send?phone=+549379480-1475&text=${mensajeCompleto}`;
+//         btnConfirmar.click()
+//         },700)
+                 
+//     });
+// }
+
+// function obtenerTotalCarrito() {
+//     return totalCarrito = carrito.reduce((acumulador, elemento) => {
+//         return acumulador + elemento.precioTotal;
+//     }, 0);
+// }
+
 let mensajeSaludo = "Hola mi pedido es:%0A%0A";
 let mensajeDetalle = "";
 let mensajeCompleto = "";
@@ -8,50 +68,41 @@ function confirmarCompra() {
 
     let btnConfirmarCompra = document.createElement("a");
     btnConfirmarCompra.classList.add("confirmar-compra");
-    btnConfirmarCompra.textContent = "Confirmar Compra ✓"
-    btnConfirmarCompra.href = "#"
+    btnConfirmarCompra.textContent = "Confirmar Compra ✓";
+    btnConfirmarCompra.href = "#";
 
     contenedor.appendChild(btnConfirmarCompra);
 
-    detalleCompra(btnConfirmarCompra);
-}
-
-function detalleCompra(btnConfirmar) {
-    btnConfirmar.addEventListener('click', () => {
-    
-        if(!mensajeEnviado) {
+    btnConfirmarCompra.addEventListener('click', () => {
+        if (!mensajeEnviado) {
             mensajeDetalle = "";
 
-        carrito.forEach(prod => {
-            mensajeDetalle += `*${prod.nombreProducto}*%0A${prod.cantidad} x $${prod.precio} - subTotal: $${prod.precioTotal}%0A%0A`;
-        });
+            carrito.forEach(prod => {
+                mensajeDetalle += `*${prod.nombreProducto}*%0A${prod.cantidad} x $${prod.precio} - subTotal: $${prod.precioTotal}%0A%0A`;
+            });
 
-        const obtenerTotal = obtenerTotalCarrito()
+            const obtenerTotal = obtenerTotalCarrito();
 
-        mensajeCompleto = `${mensajeSaludo}${mensajeDetalle}%0A%0ATotal: $${obtenerTotal}`;
+            mensajeCompleto = `${mensajeSaludo}${mensajeDetalle}%0A%0ATotal: $${obtenerTotal}`;
 
-        console.log(mensajeCompleto);
+            console.log(mensajeCompleto);
 
-        Swal.fire({
-            position: 'center',
-            icon: 'success',
-            title: 'Pedido Confirmado!',
-            showConfirmButton: false,
-            timer: 500
-        })
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Pedido Confirmado!',
+                showConfirmButton: false,
+                timer: 500
+            });
 
-        mensajeEnviado = true; // Establecer la variable de mensajeEnviado a true para evitar el bucle de alertas
+            mensajeEnviado = true; // Establecer la variable de mensajeEnviado a true para evitar el bucle de alertas
+
+            setTimeout(() => {
+                btnConfirmarCompra.target = "_blank";
+                btnConfirmarCompra.href = `https://api.whatsapp.com/send?phone=+549379480-1475&text=${mensajeCompleto}`;
+                btnConfirmarCompra.click();
+            }, 700);
         }
-        
-        if (mensajeEnviado) { // Verificar si el mensaje aún no se ha enviado
-
-              setTimeout(() => {
-                btnConfirmar.target = "_blank"
-                btnConfirmar.href = `https://api.whatsapp.com/send?phone=+549379480-1475&text=${mensajeCompleto}`;
-                btnConfirmar.click()
-              },700)
-               
-        }        
     });
 }
 
